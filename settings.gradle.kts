1ebe616d3d473f6cef6270e7868229a14ca32d14
GitHub Action <action@github.com>
2021-09-15 09:06:49 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "vesync-mvvm-template"
---
+++ src/main/kotlin/com/github/houweiandroid/vesyncmvvmtemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.houweiandroid.vesyncmvvmtemplate.services
+
+import com.github.houweiandroid.vesyncmvvmtemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
